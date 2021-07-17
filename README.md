7febda76b038460c679b3b4cb3bcea2234c4b37b
nakao107107 <imakeanewstory@gmail.com>
2021-07-17 15:23:57 +0900

    [WIP]全てのカラムを追加 & generator ロジック一部作成

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # generator
 This code is generator for  4919 data.
+
+## ファイルを生成する
+1. `npm i`
+2. `node file_generator.js`
+3. 生成されたファイルをtemplateに移動
--- file_generator.js
+++ file_generator.js
@@ -1,24 +1,99 @@
 const fs = require("fs");
+const faker = require("faker");
+faker.locale = "ja";
 const stringify = require("csv-stringify/lib/sync");
 
+//行数調整
+const school_count = 10 //学校数
+const menu_count = 10 //1つの学校あたりのメニュー数
+
+//食品情報
+// TODO: csvから読み出せるようにする
+const foodArr = [
+  {
+    ingredient_standard_name: "＜牛乳及び乳製品＞（液状乳類）普通牛乳",
+    ingredient_local_name: "牛乳",
+    food_group_6: "2",
+    food_group_3: "赤"
+  }
+]
+
+//乱数生成
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getFoodInfo(index){
+  return foodArr[index]
+}
+
+function createRow(province, town, name){
+  return {
+    "全国地方公共団体コード": "011011",
+    "都道府県名": province,
+    "市区町村名": town,
+    "学校ID": 1,
+    "学校名": name,
+    "学年": "小学校中学年",
+    "レコードID": 1,
+    "年月日": "2019-08-01",
+    "献立ID": "",
+    "料理ID": "",
+    "料理名称": "",
+    "同一料理内通し番号": 1,
+    "食品ID": "",
+    "食品名称(成分表)": foodArr[0].ingredient_standard_name,
+    "食品名称(独自)": foodArr[0].ingredient_local_name,
+    "食品群(6群)": foodArr[0].food_group_6,
+    "食品群(3群)": foodArr[0].food_group_3,
+    "分量": getRandomInt(1000),
+    "エネルギー": getRandomInt(1000),
+    "タンパク質": getRandomInt(100)/10,
+    "タンパク質エネルギー比": "",
+    "脂質": getRandomInt(100)/10,
+    "脂質エネルギー比": "",
+    "食塩相当量": getRandomInt(20)/10,
+    "カルシウム": getRandomInt(500),
+    "マグネシウム": "",
+    "鉄": getRandomInt(20)/10,
+    "ビタミンA_ug": getRandomInt(20)/10,
+    "ビタミンA_ugRAE": "",
+    "ビタミンA_IU": "",
+    "ビタミンB1": getRandomInt(20)/100,
+    "ビタミンB2": getRandomInt(100)/100,
+    "ビタミンC": getRandomInt(3),
+    "食物繊維": getRandomInt(10)/10,
+    "亜鉛": "",
+    "炭水化物": "",
+    "糖質量": "",
+    "アレルゲン情報公開・非公開": "",
+    "アレルゲン品目": "",
+    "調理方法（料理単位）": "",
+    "調理方法（食材単位）": "",
+    "備考": ""
+  }
+}
+
 (async () => {
-  const data = [
-    {
-      id: 1,
-      name: "hoge",
-      age: 17
-    },
-    {
-      id: 2,
-      name: "fuga",
-      age: 21
-    },
-    {
-      id: 3,
-      name: "piyo",
-      age: 13
+  const data = []
+  //学校の配列を生成
+  const schoolArr = []
+  for(let i=0; i<= school_count; i++){
+      const province =  faker.address.state()
+      const town = faker.address.city()
+      const schoolName = faker.name.lastName() + "小学校"
+      schoolArr.push({
+        province: province,
+        town: town,
+        name: schoolName
+      }  
+    )
+  }
+  schoolArr.forEach((school)=>{
+    for(let i=0; i<menu_count; i++){
+      data.push(createRow(school.province, school.town, school.name))
     }
-  ];
+  })
 
   const csvData = stringify(data, { header: true });
   fs.writeFileSync("./sample.csv", csvData);
